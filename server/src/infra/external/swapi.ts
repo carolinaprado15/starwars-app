a04c3e840550e5e0b5ce301ebea8d9afcff358d5
Carolina Prado <carolina.cprado15@gmail.com>
2025-04-14 16:01:37 -0300

    server: fix SSL error for both axios clients

--- server/src/infra/external/swapi.ts
+++ server/src/infra/external/swapi.ts
@@ -4,16 +4,20 @@ import { Agent } from "https";
 const swapiClient = axios.create({
   baseURL: "https://swapi.dev/api",
   timeout: 10000,
-  httpsAgent: new Agent({ rejectUnauthorized: false })
+  httpsAgent: new Agent({ rejectUnauthorized: false }),
 });
 
 const fetchResource = async (resource: string, query?: string) => {
-  const { data } = await swapiClient.get(resource, { params: { search: query } });
+  const { data } = await swapiClient.get(resource, {
+    params: { search: query },
+  });
   return data;
 };
 
 const fetchUrl = async (url: string) => {
-  const { data } = await axios.get(url);
+  const { data } = await axios.get(url, {
+    httpsAgent: new Agent({ rejectUnauthorized: false }),
+  });
   return data;
 };
 
